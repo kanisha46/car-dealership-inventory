08e5dc174caf617c6dd20596dbcaa74717872143
kanisha jasoliya <kanishajasoliya58@gmail.com>
2026-07-12 02:05:29 +0530

    test(vehicle): add failing test for vehicle creation endpoint

---
+++ carinventory_backend/src/test/java/com/incubytes/carinventory/controller/VehicalControllerTest.java
@@ -0,0 +1,41 @@
+package com.incubytes.carinventory.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class VehicleControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void shouldCreateVehicle() throws Exception {
+
+        String vehicle = """
+                {
+                    "brand":"Toyota",
+                    "model":"Fortuner",
+                    "year":2023,
+                    "price":4200000
+                }
+                """;
+
+        mockMvc.perform(post("/vehicles")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(vehicle))
+                .andExpect(status().isCreated());
+    }
+}
