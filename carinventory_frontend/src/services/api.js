42047933b78036a764a3c0c726e37c0fe4671a08
kanisha jasoliya <kanishajasoliya58@gmail.com>
2026-07-12 21:37:59 +0530

    fix: correct vercel.json

--- carinventory_frontend/src/services/api.js
+++ carinventory_frontend/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '',
+  baseURL: "https://car-dealership-inventory-wq1a.onrender.com",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
